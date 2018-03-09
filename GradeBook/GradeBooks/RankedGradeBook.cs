39e9eac5a75cf793dcc4f8fd79e8d22eefbc19f1
Michael Whitfield <elctgames67@yahoo.com>
2018-03-09 11:50:12 -0500

    Calculate grade of student

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -12,8 +13,21 @@ namespace GradeBook.GradeBooks
     public override char GetLetterGrade(double averageGrade){
         if(Students.Count < 5)
             throw new InvalidOperationException("Ranked Grading requires a minimum of 5 students to work.");
-        
-        return 'F';
+
+        var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+        var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+        if (grades[threshold-1] <= averageGrade)
+            return 'A';
+        else if (grades[(threshold*2)-1] <= averageGrade)
+            return 'B';
+        else if (grades[(threshold*3)-1] <= averageGrade)
+            return 'C';
+        else if (grades[(threshold*4)-1] <= averageGrade)
+            return 'D';
+        else
+            return 'F';
     }
   }
 }
